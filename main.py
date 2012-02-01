455e40813bdd706a74eecbbc2c8ececb21b1e210
Julian Harley <julian.harley@gmail.com>
2012-02-01 00:04:56 +0000

    hello world with jinja

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import webapp2
+#from webapp2_extras import jinja2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 from google.appengine.api import users
 from models import Greeting
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        u = users.get_current_user()
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
-        g = Greeting(content = 'hi')
-        g.put()
-        self.response.out.write(u)
-        self.response.out.write(g.content)
+        guestbook_name=self.request.get('guestbook_name', 'default')
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'guestbook_name': guestbook_name,
+            'url': url,
+            'url_linktext': url_linktext,
+            }
+
+        template = jinja_environment.get_template('templates/_base.html')
+        self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                                                debug=True)
--- models.py
+++ models.py
@@ -1,8 +1,4 @@
 from google.appengine.ext.ndb import model
-import datetime
-
-from google.appengine.ext.webapp import util
-from ndb import model, context
 
 class Greeting(model.Model):
     content = model.StringProperty()
