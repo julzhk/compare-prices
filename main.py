9f3927424b3993ce808575851db04b539eed2e96
Julian Harley <julian.harley@gmail.com>
2012-02-01 10:09:43 +0000

    add models

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import os
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 from google.appengine.api import users
-from models import Greeting
+from models import *
+
+class init(webapp2.RequestHandler):
+    def get(self):
+        for m in [Page,Site, Prodct, PriceInstance]:
+            for i in m.query().fetch():
+                i.delete()
+        print 'ok, nuked'
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        s = Site(url = 'example')
+        s.put()
+        p = Page(name = 'new page')
+        p.site = s.key
+        p.put()
         guestbook_name=self.request.get('guestbook_name', 'default')
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -26,5 +39,7 @@ class MainPage(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/_base.html')
         self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
-                                               debug=True)
+app = webapp2.WSGIApplication([
+        ('/init', init),
+        ('/', MainPage),
+    ],debug=True)
--- models.py
+++ models.py
@@ -1,5 +1,21 @@
 from google.appengine.ext.ndb import model
 
-class Greeting(model.Model):
-    content = model.StringProperty()
+class Product(model.Model):
+    name = model.StringProperty()
+    sku = model.StringProperty()
+    our_price = model.StringProperty()
+    our_url = model.StringProperty()
+
+class Site(model.Model):
+    name = model.StringProperty(indexed=True)
+    url = model.StringProperty()
+
+class Page(model.Model):
+    url = model.StringProperty(indexed=True)
+    site = model.KeyProperty()
+    price_xpath = model.KeyProperty()
+
+class PriceInstance(model.Model):
     date = model.DateTimeProperty(auto_now_add=True)
+    page = model.KeyProperty
+    price = model.FloatProperty()
