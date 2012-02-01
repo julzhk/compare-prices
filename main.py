fb8373646c7c4145737c846ee80dbde4df327be6
Julian Harley <julian.harley@gmail.com>
2012-02-01 22:23:20 +0000

    do product and price output

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import webapp2
 #from webapp2_extras import jinja2
 import jinja2
 import os
+import random
+from itertools import *
+from operator import itemgetter
+import pprint
+
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 from google.appengine.api import users
@@ -9,29 +14,47 @@ from models import *
 
 class init(webapp2.RequestHandler):
     def get(self):
-        for m in [Page,Site, Product, PriceInstance]:
+        for m in [Page,Site, Product]:
             print '*'*8, m
             for i in m.query():
-                print i
-                del(i)
+                i.key.delete()
         print 'ok, nuked'
         for name in ['bbq1','bbq2','bbq3']:
-            p = Product(name = name)
+            p = Product(name = name,our_price = 20+ random.random())
             p.put()
-
+            page = Page(url = 'amazon.com',product = p.key,
+                        current_price = 23.3+ random.random())
+            page.put()
+            page = Page(url = 'ebay.com',product = p.key,
+                        current_price = 15+ random.random())
+            page.put()
 
 
 class MainPage(webapp2.RequestHandler):
+    def grouper(self,data):
+        data.sort()
+        r = []
+        print 'Grouped, sorted:'
+        for k, g in groupby(data, lambda o:o.product):
+#            print k
+            p = Product.query(Product.key== k).get()
+#            print p.name,':'
+            for a, b in groupby(list(g), lambda o:o.url):
+#                print a
+                for c in b:
+#                    print c.url, ' $ ',c.current_price
+                    c.product_data = p
+                    r.append(c)
+        r = sorted(r, key=lambda p: p.product.urlsafe())
+        return r
+
     def get(self):
-        allproducts = Product.query()
-        showproduct = self.request.get('product', None)
-        if showproduct:
-            showproduct = Product.query(Product.name == showproduct).get()
-
-
+        allproducts = Product.query().fetch()
+        allpages = Page.query().order(Page.product).fetch()
+        product_list = self.grouper(data=allpages)
         template_values = {
             'products':allproducts,
-            'showproduct':showproduct,
+            'prices': product_list,
             }
 
         template = jinja_environment.get_template('templates/main.html')
--- models.py
+++ models.py
@@ -3,19 +3,17 @@ from google.appengine.ext.ndb import model
 class Product(model.Model):
     name = model.StringProperty()
     sku = model.StringProperty()
-    our_price = model.StringProperty()
-    our_url = model.StringProperty()
+    our_price = model.FloatProperty()
+    our_url = model.FloatProperty()
 
 class Site(model.Model):
     name = model.StringProperty(indexed=True)
     url = model.StringProperty()
 
-class Page(model.Model):
+class Page(model.Expando):
     url = model.StringProperty(indexed=True)
     site = model.KeyProperty()
+    product = model.KeyProperty()
     price_xpath = model.KeyProperty()
-
-class PriceInstance(model.Model):
     date = model.DateTimeProperty(auto_now_add=True)
-    page = model.KeyProperty
-    price = model.FloatProperty()
+    current_price = model.FloatProperty()
