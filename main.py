3f7ce6d5afbaa5bd62dda118d00d27448df9d5d4
Julian Harley <julian.harley@gmail.com>
2012-02-06 14:02:23 +0000

    Simple scraping functionality

--- main.py
+++ main.py
@@ -1,30 +1,149 @@
 import webapp2
-#from webapp2_extras import jinja2
 import jinja2
 import os
-
+import random
+from itertools import *
+from operator import itemgetter
+from BeautifulSoup import BeautifulSoup
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-from google.appengine.api import users
-from models import Greeting
+from models import Product, Site, Page, Archive_Price, acme, betta_stuff
+
+class init(webapp2.RequestHandler):
+    '''
+        Simple wipe & re-initialize the datastore.
+        Obviously, this will be deleted in the live site.
+    '''
+
+    def get(self):
+        for m in [Page,Site, Product,Archive_Price]:
+            for i in m.query():
+                i.key.delete()
+        self.response.out.write('ok, nuked')
+        t = Site(
+            name = 'acme.com',
+            price_class= 'acme',
+            url = 'http://www.acme.com'
+        )
+        t.put()
+        for name, url in [
+            ('Product A','http://www.example.com?id=1'),
+            ('Product B','http://www.example.com?id=2'),
+            ('Product C','http://www.example.com?id=3')
+        ]:
+            product = Product(
+                name = name,
+                our_price = 20+ random.random(),
+                sku = name
+            )
+            product.put()
+            page = Page(url =url,
+                        product = product.key,
+                        site = t.key,
+                        current_price = 99.9)
+            page.put()
+        t = Site(
+            name = 'betta stuff',
+            price_class= 'betta_stuff',
+            url = 'http://betta_stuff.example.com'
+        )
+        t.put()
+        for name, url in [
+            ('Product A','http://betta_stuff.example.com?id=5'),
+            ('Product B','http://betta_stuff.example.com?id=6'),
+            ('Product D','http://betta_stuff.example.com?id=9')
+        ]:
+            product = Product(name = name,our_price = 20+ random.random(),sku = name)
+            product.put()
+            page = Page(url =url,
+                        product = product.key,
+                        site = t.key,
+                        current_price = 99.9)
+            page.put()
+
+
+class update(webapp2.RequestHandler):
+    '''
+        Called by the google app engine CRON
+    '''
+
+    def store_archive(self,page):
+        '''
+            On any price update simply put the old data into archive store.
+        '''
+        site = Site.query(Site.key == page.site).get()
+        a = Archive_Price(
+            product = page.product,
+            date=page.date,
+            price =page.current_price,
+            url = page.url,
+            site = site.name
+        )
+        a.put()
+
+    def get(self):
+        '''
+            Update all the prices, using the 'price_class' model's beautiful
+            soup  to scrape
+        '''
+        allpages = Page.query().order(Page.date).fetch()
+        for page in allpages:
+            site = Site.query(Site.key == page.site).get()
+            g = eval(site.price_class)()
+            page.current_price = g.get_price(url = page.url)
+            self.store_archive(page)
+            page.put()
+        self.response.out.write('pages scraped and updated')
+
 
 class MainPage(webapp2.RequestHandler):
+    '''
+        Main page. Uses Itertools to group up the data neatly
+    '''
+    def grouper(self,data):
+        data.sort()
+        r = []
+        for product_key, outer_grouper in groupby(data, lambda o:o.product):
+            p = Product.query(Product.key== product_key).get()
+            for site_name, inner_grouper in groupby(list(outer_grouper), lambda o:o.url):
+                for page in inner_grouper:
+                    page.product_data = p
+                    s = Site.query(Site.key == page.site).get()
+                    page.site_name = s.name
+                    r.append(page)
+        r = sorted(r, key=lambda p: p.product_data.name)
+        return r
+
     def get(self):
-        guestbook_name=self.request.get('guestbook_name', 'default')
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
+        allproducts = Product.query().fetch()
+        allpages = Page.query().order(Page.product).fetch()
+        product_list = self.grouper(data=allpages)
         template_values = {
-            'guestbook_name': guestbook_name,
-            'url': url,
-            'url_linktext': url_linktext,
+            'products':allproducts,
+            'pages': product_list,
             }
-
-        template = jinja_environment.get_template('templates/_base.html')
+        template = jinja_environment.get_template('templates/main.html')
         self.response.out.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage)],
-                                               debug=True)
+
+class archive(webapp2.RequestHandler):
+    '''
+        Show the archive: just a list currently (chart to follow)
+    '''
+    def get(self):
+        k = self.request.get('product')
+        product = Product.get_by_id(int(k))
+        archive_data = Archive_Price.query(Archive_Price.product == product.key).fetch()
+        template_values = {
+            'product':product,
+            'data': archive_data,
+            }
+        template = jinja_environment.get_template('templates/archive.html')
+        self.response.out.write(template.render(template_values))
+
+
+app = webapp2.WSGIApplication([
+        ('/init', init),
+        ('/get', update),
+        ('/', MainPage),
+        ('/archive', archive),
+    ],debug=True)
--- models.py
+++ models.py
@@ -1,5 +1,76 @@
 from google.appengine.ext.ndb import model
+from BeautifulSoup import BeautifulSoup
+import re
 
-class Greeting(model.Model):
-    content = model.StringProperty()
+
+class Product(model.Model):
+    name = model.StringProperty()
+    sku = model.StringProperty()
+    our_price = model.FloatProperty()
+    our_url = model.FloatProperty()
+
+
+class Site(model.Model):
+    name = model.StringProperty(indexed=True)
+    price_class = model.StringProperty()
+    url = model.StringProperty()
+
+
+class Page(model.Expando):
+    url = model.StringProperty(indexed=True)
+    site = model.KeyProperty()
+    product = model.KeyProperty()
     date = model.DateTimeProperty(auto_now_add=True)
+    current_price = model.FloatProperty()
+
+
+class Archive_Price(model.Model):
+    product = model.KeyProperty()
+    date = model.DateTimeProperty()
+    price = model.FloatProperty()
+    url = model.StringProperty()
+    site = model.StringProperty()
+
+
+class retailer(object):
+    '''
+        Abstract Base Class for each online shop.
+        Each vendor has a custom get_price method, and perhaps other
+        functions, (such as shipping cost) etc.
+
+    '''
+    def get_page_content(self,url):
+        from google.appengine.api import urlfetch
+        result = urlfetch.fetch(url=url)
+        if result.status_code == 200:
+            return result.content
+
+
+class acme(retailer):
+    '''
+        A sample vendor.
+    '''
+
+    def get_price(self,url):
+        html_doc  = self.get_page_content(url =url)
+        soup = BeautifulSoup(html_doc)
+        for i in soup.findAll(color="#ff0000"):
+            for c in i.contents:
+                price = re.findall(r'[0-9\.]+', str(c))
+                if price:
+                    return float(price[0])
+
+
+class betta_stuff(retailer):
+    '''
+        A sample vendor.
+    '''
+
+    def get_price(self,url):
+        html_doc  = self.get_page_content(url =url)
+        soup = BeautifulSoup(html_doc)
+        i = soup.find(id="pricediv0")
+        c = i.contents[0]
+        price = re.findall(r'[0-9\.]+', c)
+        if price:
+            return float(price[0])
